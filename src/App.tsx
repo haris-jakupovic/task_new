ccb2b945b60a00067f8e47223ae7d4c2b23c3c49
haris-jakupovic <114880057+haris-jakupovic@users.noreply.github.com>
2023-04-10 11:00:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './styles/style.css';
-
-interface User {
-  id: number;
-  name: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-  };
-  phone: string;
-  company: {
-    name: string;
-  };
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./styles/style.css";
+import { User } from "./types/user";
+import Header from "./components/Header";
+import Table from "./components/Table";
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [sortDirection, setSortDirection] = useState<'ASC' | 'DESC'>('ASC');
+  const [sortDirection, setSortDirection] = useState<"ASC" | "DESC">("ASC");
 
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/users')
+      .get("https://jsonplaceholder.typicode.com/users")
       .then((response) => setUsers(response.data))
       .catch((error) => console.log(error));
   }, []);
 
-  const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setSearchTerm(event.target.value);
     setCurrentPage(1);
   };
 
   const handleSortDirectionChange = () => {
-    setSortDirection(sortDirection === 'ASC' ? 'DESC' : 'ASC');
+    setSortDirection(sortDirection === "ASC" ? "DESC" : "ASC");
   };
 
   const sortedUsers = [...users].sort((a, b) =>
-    sortDirection === 'ASC' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+    sortDirection === "ASC"
+      ? a.name.localeCompare(b.name)
+      : b.name.localeCompare(a.name)
   );
 
   const filteredUsers = sortedUsers.filter((user) =>
@@ -59,48 +52,29 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1>Users</h1>
-      <label htmlFor="search">Search by city:</label>
-      <input id="search" type="text" value={searchTerm} onChange={handleSearchTermChange} />
-      <div className="total-records">Total records : {totalUsers}</div>
-      <table>
-        <thead>
-          <tr>
-          <th onClick={handleSortDirectionChange}>Name{' '}
-            {sortDirection === 'ASC' ? (
-            <span>&#x25bc;</span> ) : (
-            <span>&#x25BC;</span> )}{' '}
-            {sortDirection === 'DESC' ? ( 
-            <span>&#x25b2;</span> ) : (
-            <span>&#x25B2;</span> )}
-          </th>
-            <th>Address</th>
-            <th>Phone</th>
-            <th>Company</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentUsers.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{`${user.address.street}, ${user.address.suite}, ${user.address.city}`}</td>
-              <td>{user.phone}</td>
-              <td>{user.company.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Header
+        searchTerm={searchTerm}
+        handleSearchTermChange={handleSearchTermChange}
+        totalUsers={totalUsers}
+      />
+      <Table
+        currentUsers={currentUsers}
+        handleSortDirectionChange={handleSortDirectionChange}
+        sortDirection={sortDirection}
+      />
       <div className="pagination">
-        {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
-          <button
-            key={pageNumber}
-            onClick={() => handlePageChange(pageNumber)}
-            disabled={pageNumber === currentPage}
-            className={pageNumber === currentPage ? 'active' : ''}
-          >
-            {pageNumber}
-          </button>
-        ))}
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+              disabled={pageNumber === currentPage}
+              className={pageNumber === currentPage ? "active" : ""}
+            >
+              {pageNumber}
+            </button>
+          )
+        )}
       </div>
     </div>
   );
